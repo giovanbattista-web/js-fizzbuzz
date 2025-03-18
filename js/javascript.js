4dc5ae44ffb7e8ed9f640b5dcb3042159a5a55de
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-18 15:09:49 +0100

    [UPD] STAMPA DEI NUMERI DA 1 A 100

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,7 @@
 // DICHIARAZIONE DELLE VARIABILI ED INSERIMENTO DEI VALORI
 let numeroDaInserire = parseInt(prompt("Inserisci i numeri da inserire"));
+
+// STAMPA DEI NUMERI DA 1 A 100
+for (let i=1;i<=100;i++){
+                       console.log(i);
+}

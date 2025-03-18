0a5307f65ae2969ea615b7482c064da69c897fca
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-18 16:13:37 +0100

    [UPD] STAMPA DI FIZZ E BUZZ

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,16 @@
 // DICHIARAZIONE DELLE VARIABILI ED INSERIMENTO DEI VALORI
-let numeroDaInserire = parseInt(prompt("Inserisci i numeri da inserire"));
 
 // STAMPA DEI NUMERI DA 1 A 100
-for (let i=1;i<=100;i++){
-                       console.log(i);
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0) {
+        console.log(`${i} Fizz`);
+    }
+    else
+        if (i % 5 == 0) {
+            console.log(`${i} Buzz`);
+        }
+        
+
+
 }

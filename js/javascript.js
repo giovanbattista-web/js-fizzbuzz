92987fa817bb7f85ea254b7cf2a0ede42897fcf4
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-18 17:09:04 +0100

    [UPD] DICHIARAZIONE DI VARIABILI, INSERIMENTO DI VALORI E README.MD

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,18 @@
 // DICHIARAZIONE DELLE VARIABILI ED INSERIMENTO DEI VALORI
-
+let multiploTreCinque = "FizzBuzz";
+let multiploTre = "Fizz";
+let multiploCinque = "Buzz";
 // STAMPA DEI NUMERI DA 1 A 100
 for (let i = 1; i <= 100; i++) {
     if ((i % 3 == 0) && (i % 5 == 0)) {
-                     console.log(`FizzBuzz`);
+                     console.log(multiploTreCinque);
     }
     else if (i % 3 == 0) {
-        console.log(`Fizz`);
+        console.log(multiploTre);
     }
     else
         if (i % 5 == 0) {
-            console.log(`Buzz`);
+            console.log(multiploCinque);
         }
         else {
             console.log(i);
